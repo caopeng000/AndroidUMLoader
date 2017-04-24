569f4e5fb4eac737a25c0a9fcd27ae3c3c8a2139
caopeng <caopeng@kaolafm.com>
2017-04-24 10:43:15 +0800

    新建项目

---
+++ app/src/main/java/com/androidumloader/UILApplication.java
@@ -0,0 +1,13 @@
+package com.androidumloader;
+
+/******************************************
+ * 类名称：UILApplication
+ * 类描述：
+ *
+ * @version: 2.3.1
+ * @author: caopeng
+ * @time: 2017/4/24 10:41
+ ******************************************/
+public class UILApplication {
+
+}
